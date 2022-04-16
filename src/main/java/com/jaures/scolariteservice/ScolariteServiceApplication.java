28ae19cd0512ba7aa66b31e3676c1db74ac0335b
beinjamin <jauresbeinjamin@gmail.com>
2022-04-16 21:29:57 +0100

    Initialisation

---
+++ src/main/java/com/jaures/scolariteservice/ScolariteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jaures.scolariteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScolariteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScolariteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaures/scolariteservice/ScolariteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaures.scolariteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScolariteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
