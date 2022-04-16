8212752a72f96af582226596d139a90842980599
beinjamin <jauresbeinjamin@gmail.com>
2022-04-16 22:06:06 +0100

    Creation de la class

--- src/main/java/com/jaures/scolariteservice/ScolariteServiceApplication.java
+++ src/main/java/com/jaures/scolariteservice/ScolariteServiceApplication.java
@@ -1,7 +1,24 @@
 package com.jaures.scolariteservice;
 
+import javax.persistence.GenerationType;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@Entity
+class Student {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private Date birthDate;
+}
+
+
+
+
 
 @SpringBootApplication
 public class ScolariteServiceApplication {
