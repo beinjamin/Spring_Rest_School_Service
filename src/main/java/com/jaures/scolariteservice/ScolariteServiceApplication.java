4ef5f8cb3d7a14eafa0b4bd30995a9a1b17be611
beinjamin <jauresbeinjamin@gmail.com>
2022-04-16 22:20:22 +0100

    Creation d'un etudiant

--- src/main/java/com/jaures/scolariteservice/ScolariteServiceApplication.java
+++ src/main/java/com/jaures/scolariteservice/ScolariteServiceApplication.java
@@ -7,8 +7,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -43,6 +45,13 @@ public class ScolariteServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScolariteServiceApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner start(StudentRepository studentRepository ) {
+		return args -> {
+			studentRepository.save(new Student(null,"Jaures","jauresbeinjamin@gmail.com",mew Date()));
+			
+		};
 	}
 
 }
