566cb5b6a5749206a42765556f52202b35f13498
beinjamin <jauresbeinjamin@gmail.com>
2022-04-17 00:56:40 +0100

    Insertion du RequestBody pour que le systeme sache que la requette vien du fichier json

--- src/main/java/com/jaures/scolariteservice/ScolariteServiceApplication.java
+++ src/main/java/com/jaures/scolariteservice/ScolariteServiceApplication.java
@@ -22,6 +22,7 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -70,8 +71,8 @@ class ScolariteRestController{
 		return studentRepository.findById(id).get();
 		
 	}
-	@PostMapping("/students")
-	public Student save(Student student) {
+	@PostMapping("/studentsss")
+	public Student save(@RequestBody Student student) {
 		return studentRepository.save(student);
 	}
 }
