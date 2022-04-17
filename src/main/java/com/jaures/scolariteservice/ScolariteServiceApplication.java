2fa44c4db7e8044b699928d62115b5b8ea632cd0
beinjamin <jauresbeinjamin@gmail.com>
2022-04-17 01:00:53 +0100

    Mise en place put pour la mise a jour d'un etudiant

--- src/main/java/com/jaures/scolariteservice/ScolariteServiceApplication.java
+++ src/main/java/com/jaures/scolariteservice/ScolariteServiceApplication.java
@@ -22,6 +22,7 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -75,8 +76,13 @@ class ScolariteRestController{
 	public Student save(@RequestBody Student student) {
 		return studentRepository.save(student);
 	}
+	@PutMapping("/studentsss")
+	public Student update(@PathVariable(name="id") Long id, @RequestBody Student student){
+		
+	student.setId(id);
+		return studentRepository.save(student);
+	}
 }
-
 
 
 @SpringBootApplication
